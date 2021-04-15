00c453838394141024894a1883b7b835a9759d10
mostafa-farrage <49571930+mostafa-farrage@users.noreply.github.com>
2021-04-15 07:29:55 +0200

    index

--- Workshop/WorkshopPlatform/Controllers/HomeController.cs
+++ Workshop/WorkshopPlatform/Controllers/HomeController.cs
@@ -36,5 +36,9 @@ namespace WorkshopPlatform.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult TopRated()
+        {
+            return View();
+        }
     }
 }
